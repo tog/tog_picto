830f92dc5fafc9d9c600ebcafd7a8c40488243e0
Balint Erdi <balint.erdi@gmail.com>
2009-03-10 01:00:01 +0800

    photos should be deleted when their user is
    Signed-off-by: Balint Erdi <balint.erdi@gmail.com>
    
    Signed-off-by: Roberto Salicio <roberto.salicio@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_many :photos, :class_name => "Picto::Photo"
+  has_many :photos, :class_name => "Picto::Photo", :dependent => :destroy
   has_many :owned_photosets, :class_name => "Picto::Photoset"
   has_many :photoset_memberships, :class_name => "Picto::PhotosetMembership"
   has_many :joined_photosets, :class_name => "Picto::Photoset", :through => :photoset_memberships, :source => :user

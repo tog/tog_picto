86660791041c005ed507ed25efca9b962a716c24
Alberto Molpeceres <alberto.molpeceres@gmail.com>
2009-05-03 21:11:33 +0200

    calculate number of photos for the user, by balint erdi

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,12 @@ class User < ActiveRecord::Base
   has_many :owned_photosets, :class_name => "Picto::Photoset"
   has_many :photoset_memberships, :class_name => "Picto::PhotosetMembership"
   has_many :joined_photosets, :class_name => "Picto::Photoset", :through => :photoset_memberships, :source => :user
-  
+
   def public_photosets
     owned_photosets.delete_if {|set| !set.authorized(nil, :read)}
+  end
+
+  def number_of_photos
+    owned_photosets.inject(0) { |sum, photoset| sum + photoset.number_of_photos }
   end
 end
